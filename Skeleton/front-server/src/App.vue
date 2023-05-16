5c77769fee89cdf49c099dbecc5c010773ed8ef5
전대현 <wjseogus523@naver.com>
2023-05-16 16:19:34 +0900

    5/16 gitignore 적용

--- Skeleton/front-server/src/App.vue
+++ Skeleton/front-server/src/App.vue
@@ -1,9 +1,17 @@
 <template>
-  
-  <v-main>
-    <router-view/>
-  </v-main>
-  
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title>Navigation</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text>Link 1</v-btn>
+      <v-btn text>Link 2</v-btn>
+      <v-btn text>Link 3</v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <!-- 나머지 앱 내용 -->
+    </v-main>
+  </v-app>
 </template>
 
 <script>
@@ -24,3 +32,13 @@ export default {
   }
 };
 </script>
+
+<style>
+.v-toolbar-title {
+  font-size: 18px;
+}
+
+.v-btn {
+  margin-left: 8px;
+}
+</style>
--- Skeleton/front-server/src/components/DepositList.vue
+++ Skeleton/front-server/src/components/DepositList.vue
@@ -1,14 +1,20 @@
 <template>
-  <v-flex class="dataList" column align-center>
-    <v-card max-width='600' tile>
-      <v-list-item v-for="(data, index) in propsdata.slice(0, 15)" :key='index'>
-        <v-list-item-content>
-            <v-list-item-title>은행명: {{data.kor_co_nm}}</v-list-item-title>
-            <v-list-item-subtitle>상품이름: {{data.fin_prdt_nm}}</v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-    </v-card>
-  </v-flex>
+  <v-container class="dataList" fluid v-if="propsdata && propsdata.length > 0">
+    <v-row justify="center">
+      <v-col cols="12" sm="6" md="4" v-for="(data, index) in propsdata.slice(0, 15)" :key="index">
+        <v-card class="list-card" outlined>
+          <v-card-text>
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title>은행명: {{ data.kor_co_nm }}</v-list-item-title>
+                <v-list-item-subtitle>상품이름: {{ data.fin_prdt_nm }}</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -20,7 +26,11 @@ export default {
 
 <style>
 .dataList {
-  text-align: start;
   margin-top: 100px;
+}
+
+.list-card {
+  max-width: 300px;
+  margin: 10px;
 }
 </style>
