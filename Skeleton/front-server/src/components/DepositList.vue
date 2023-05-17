fd312d545b9fd607ebeeca95950f5b31be8e2072
전대현 <wjseogus523@naver.com>
2023-05-17 17:46:28 +0900

    5월17일 -> 적금 모델 추가, 예금 목록 구현

--- Skeleton/front-server/src/components/DepositList.vue
+++ Skeleton/front-server/src/components/DepositList.vue
@@ -1,22 +1,20 @@
 <template>
   <v-container class="dataList" fluid v-if="propsdata && propsdata.length > 0">
-    
-    <v-card class="list-card" outlined>
+    <v-card class="list-card" outlined >
       <v-card-text>
-        <h2 class="list-title">예금 List</h2>
-        <v-data-table :headers="headers" :items="flattenData">
-          <template v-slot:item="{ item }">
-            <tr>
-              <td>{{ item.kor_co_nm }}</td>
-              <td>{{ item.fin_prdt_nm }}</td>
-              <td>{{ item.save_trm }} 개월</td>
-              <td>{{ item.intr_rate }}</td>
-              <td>{{ item.join_member }}</td>
-              <td>{{ item.join_way }}</td>
-              <td>{{ item.etc_note }}</td>
-            </tr>
-          </template>
-        </v-data-table>
+        <h2 class="list-title">예금 List</h2> <br>
+        <div class="table-container">
+          <v-data-table :headers="headers" :items="flattenData">
+            <template v-slot:item="{ item }">
+              <tr>
+                <td>{{ item.kor_co_nm }}</td>
+                <td>{{ item.fin_prdt_nm }}</td>
+                <td>{{ item.save_trm }} 개월</td>
+                <td>{{ item.intr_rate }}</td>
+              </tr>
+            </template>
+          </v-data-table>
+        </div>
       </v-card-text>
     </v-card>
   </v-container>
@@ -35,24 +33,20 @@ export default {
         { text: '상품이름', value: 'fin_prdt_nm' },
         { text: '기간', value: 'save_trm' },
         { text: '금리', value: 'intr_rate' },
-        { text: '가입 조건', value: 'join_member' },
-        { text: '가입 방법', value: 'join_way' },
-        { text: '유의사항', value: 'etc_note' },
-      ];
+      ].map(header => ({ ...header, align: 'center' }));
     },
     flattenData() {
       const flattenedData = [];
       this.propsdata.forEach(data => {
         data.additionalData.forEach(item => {
           flattenedData.push({
-            
             kor_co_nm: data.kor_co_nm,
             fin_prdt_nm: data.fin_prdt_nm,
             save_trm: item.save_trm,
             intr_rate: item.intr_rate,
             join_member: data.join_member,
             join_way: data.join_way,
-            etc_note : data.etc_note,
+            etc_note: data.etc_note,
           });
         });
       });
@@ -64,18 +58,17 @@ export default {
 
 <style>
 .dataList {
-  margin-top: 100px;
+  margin-top: 150px;
   display: flex;
   justify-content: center;
 }
 
 .list-card {
-  max-width: 2000px;
-  max-height : 2000px;
+  max-width: 100%;
   margin: 10px;
 }
 
-.v-data-table {
+.table-container {
   max-height: 400px;
   overflow-y: auto;
 }
@@ -83,5 +76,16 @@ export default {
 .list-title {
   text-align: center;
   margin-top: 0;
+}
+
+/* 반응형 스타일 */
+@media (max-width: 600px) {
+  .list-card {
+    max-height: unset;
+  }
+  
+  .table-container {
+    max-height: unset;
+  }
 }
 </style>
